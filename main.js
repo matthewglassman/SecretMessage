d22c5c8747933b656d3f3439d8eb9ed53dc3571f
Matthew Glassman <matthewglassman78@gmail.com>
2022-03-05 19:59:05 -0500

    Using pop method on the array to remove the last entry.

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage.length);
