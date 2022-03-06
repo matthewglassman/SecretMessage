66d9af21b4535490e9a5da147baa6ca8839536c9
Matthew Glassman <matthewglassman78@gmail.com>
2022-03-05 20:05:58 -0500

    Replacing one word in the array with another by accessing the index.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
-console.log(secretMessage.length);
+//console.log(secretMessage.length);
 secretMessage.pop();
-console.log(secretMessage.length);
+//console.log(secretMessage.length);
 
 secretMessage.push('to', 'Program');
+//console.log(secretMessage);
+
+secretMessage[7]='right';
 console.log(secretMessage);
